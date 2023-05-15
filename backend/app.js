4b84c43d55345e33c681720baa065b4df98287f8
Alena Elegecheva <alena.elegecheva@gmail.com>
2023-05-15 12:07:15 +0700

    Исправить корс

--- backend/app.js
+++ backend/app.js
@@ -8,39 +8,16 @@ const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const helmet = require('helmet');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const cors = require('cors');
 const rootRoute = require('./routes/index');
 const genErrorHandler = require('./middlewares/genErrorHandler');
 const limiter = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
-const allowedCors = [
-  'https://elegant.mesto.nomoredomains.monster',
-  'http://elegant.mesto.nomoredomains.monster',
-  'https://api.elegant.mesto.nomoredomains.monster',
-  'http://api.elegant.mesto.nomoredomains.monster',
-  'localhost:3000',
-  'localhost:3002',
-];
+const app = express();
+app.use(cors);
 
 const { PORT = 3000 } = process.env;
-const app = express();
-
-app.options(
-  '*',
-  cors({
-    origin: allowedCors,
-    credentials: true,
-  }),
-);
-
-app.use(
-  cors({
-    origin: allowedCors,
-    credentials: true,
-  }),
-);
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
@@ -52,7 +29,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(limiter);
-// app.use(cors);
 app.use(requestLogger); // подключаем логгер запросов
 app.use('/', rootRoute);
 app.use(errorLogger); // подключаем логгер ошибок
